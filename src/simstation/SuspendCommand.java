4ba4193d1794020a9decfd7b618f906c962ce3f6
Arav Dharmesh Panchmatia <136196166+BinaryAP@users.noreply.github.com>
2024-04-12 14:44:54 -0700

    Wrote SuspendCommand.java

--- src/simstation/SuspendCommand.java
+++ src/simstation/SuspendCommand.java
@@ -1,4 +1,15 @@
 package simstation;
 
-public class SuspendCommand {
+import mvc.*;
+
+public class SuspendCommand extends Command {
+
+    public SuspendCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation simulation = (Simulation) model;
+        simulation.suspend();
+    }
 }
