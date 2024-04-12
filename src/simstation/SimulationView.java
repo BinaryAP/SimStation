8fbac0f902970aa58026886a76cfc96c53781e3d
Arav Dharmesh Panchmatia <136196166+BinaryAP@users.noreply.github.com>
2024-04-12 00:01:04 -0700

    Wrote SimulationView.java

--- src/simstation/SimulationView.java
+++ src/simstation/SimulationView.java
@@ -1,4 +1,33 @@
 package simstation;
 
-public class SimulationView {
+import mvc.*;
+import java.awt.*;
+
+public class SimulationView extends View {
+
+    protected Graphics gc;
+    Simulation simulation;
+    public SimulationView(Simulation simulation) {
+        super(simulation);
+        this.simulation = simulation;
+    }
+
+    public void update() {
+        Simulation simulation = (Simulation) model;
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+
+        for (Agent agent : simulation.getAgents()) {
+            int x = agent.getX();
+            int y = agent.getY();
+            int size = agent.getSize();
+
+            gc.drawOval(x, y,size,size);
+            gc.fillOval(x, y, size, size);
+        }
+    }
 }
