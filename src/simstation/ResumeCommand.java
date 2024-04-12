eb13640cc6ac49777d9e6c0bda7a9891a79c1dc1
Arav Dharmesh Panchmatia <136196166+BinaryAP@users.noreply.github.com>
2024-04-12 14:44:10 -0700

    Wrote ResumeCommand.java

--- src/simstation/ResumeCommand.java
+++ src/simstation/ResumeCommand.java
@@ -1,4 +1,15 @@
 package simstation;
 
-public class ResumeCommand {
+import mvc.*;
+
+public class ResumeCommand extends Command {
+
+    public ResumeCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation simulation = (Simulation) model;
+        simulation.resume();
+    }
 }
