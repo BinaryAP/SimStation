fa1a0a007c95ab8a02491c14784486278bb6774d
Justin <justlee888@gmail.com>
2024-04-13 11:58:06 -0700

    should work

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -1,9 +1,8 @@
 package mvc;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class AppPanel extends JPanel implements Subscriber, ActionListener  {
 
@@ -84,7 +83,6 @@ public class AppPanel extends JPanel implements Subscriber, ActionListener  {
             } else if (cmmd.equals("New")) {
                 Utilities.saveChanges(model);
                 setModel(factory.makeModel());
-
                 model.setUnsavedChanges(false);
             } else if (cmmd.equals("Quit")) {
                 Utilities.saveChanges(model);
