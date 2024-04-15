9227d0b807bccbb6435dd4de005700b3d357b457
Justin <justlee888@gmail.com>
2024-04-15 15:33:03 -0700

    finished

--- src/flocking/Flocking.java
+++ src/flocking/Flocking.java
@@ -2,18 +2,25 @@ package flocking;
 
 import mvc.*;
 import simstation.*;
-import java.awt.*;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class Flock extends Agent {
+    public int steps;
 
     public Flock() {
         super();
-
+        heading = Heading.random();
+        steps = Utilities.rng.nextInt(5) + 1;
     }
 
     public void update() {
-
+        Agent neighbor =  world.getNeighbor(this, 40);
+        if(neighbor != null){
+            steps = ((Flock) neighbor).steps;
+            heading = neighbor.heading;
+        }
+        move(steps);
     }
 
 }
@@ -34,6 +41,25 @@ public class Flocking extends Simulation {
     public void populate() {
         for(int i = 0; i < 15; i++)
             addAgent(new Flock());
+    }
+    public String getStats(){
+        ArrayList<Agent> agents = getAgents();
+        HashMap<Integer, Integer> speed = new HashMap<Integer,Integer>();
+
+        for (Agent agent : agents) {
+            Flock flock = (Flock) agent;
+
+            if (!speed.containsKey(flock.steps)) {
+                speed.put(flock.steps, 1);
+            } else {
+                speed.put(flock.steps, speed.get(flock.steps) + 1);
+            }
+        }
+        final String[] result = {""};
+        speed.forEach((key,value)->{
+            result[0] +=("#birds @ speed " + key.toString() + " = " + value + "\n");
+        });
+        return result[0];
     }
 
     public static void main(String[] args) {
